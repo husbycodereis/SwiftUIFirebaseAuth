92cf66470ab9f3c8047bf6f1ba6684bac5ace360
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-04-06 12:03:34 +0300

    Initial Commit

---
+++ SwiftUIFirebaseAuth/SwiftUIFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAuthApp.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Ali Riza Reisoglu on 6.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
